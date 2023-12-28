ee4e20884060229dcc5e6d2ef9eb1096d1da2ea1
suralokin123 <koly_romashkin@bk.ru>
2023-12-28 18:19:09 +0300

    обновил код

--- JS/card.js
+++ JS/card.js
@@ -1,52 +1,23 @@
 
-
-
-const cardLinks = document.querySelectorAll('.information-cards a');
-
-let clickCounts = { card1: 0, card2: 0, card3: 0 };
-
-cardLinks.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    const selectedCard = e.target.getAttribute('class').slice(-1);
-    const allCards = document.querySelectorAll('.information-cards > div');
-
-    allCards.forEach(card => {
-      if (card.getAttribute('class').slice(-1) === selectedCard) {
-        card.style.display = 'block';
-        clickCounts[selectedCard]++;
-        if (clickCounts[selectedCard] === 2) {
-          openModal();
-        }
-      } else {
-        card.style.display = 'none';
-      }
-    });
-  });
-});
-
 // Функция для открытия модального окна
 function openModal() {
-  var modal = document.querySelector('.modal');
+  const modal = document.querySelector('.modal');
   modal.style.display = 'block';
   // Сброс счетчика после открытия модального окна
   clickCounts = { card1: 0, card2: 0, card3: 0 };
 }
 
 
-
-
-
 // Функция для отображения информации о карте
 function showInfo(infoId) {
   // Сначала скрываем все элементы с классом "info"
-  var infoElements = document.querySelectorAll('.info');
+  const infoElements = document.querySelectorAll('.info');
   infoElements.forEach(function(element) {
     element.style.display = 'none';
   });
 
   // Затем отображаем элемент с соответствующим ID
-  var infoElement = document.getElementById(infoId);
+  const infoElement = document.getElementById(infoId);
   infoElement.style.display = 'block';
 
   // Если этот элемент уже отображен, то открываем модальное окно
@@ -65,7 +36,7 @@ function openModal() {
 
 // Добавляем обработчик для кнопки закрытия модального окна
 document.querySelector('.close').addEventListener('click', function() {
-  var modal = document.querySelector('.modal');
+  const modal = document.querySelector('.modal');
   modal.style.display = 'none';
   // Удаляем информацию о состоянии модального окна из локального хранилища при закрытии
   localStorage.removeItem('modalOpened');
---
+++ JS/timer.js
@@ -0,0 +1,6 @@
+function showInfo(id) {
+    const modal = document.getElementById(id);
+    setTimeout(function() {
+      modal.style.display = "block";
+    }, 0); // таймер на 10 секунд
+  }
