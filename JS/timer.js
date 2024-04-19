efd4c66a583002eb383306100a93b56ea9f49b7c
suralokin123 <kolya_romashkin@bk.ru>
2024-04-19 19:15:03 +0300

    обновил код

--- JS/timer.js
+++ JS/timer.js
@@ -4,3 +4,18 @@ function showInfo(id) {
       modal.style.display = "block";
     }, 0); // таймер на 10 секунд
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.querySelector('.modal');
+    var closeBtn = document.querySelector('.close');
+  
+    closeBtn.addEventListener('click', function() {
+      modal.style.display = 'none';
+      localStorage.setItem('modalClosed', 'true');
+    });
+  
+    if (localStorage.getItem('modalClosed')) {
+      modal.style.display = 'none';
+    }
+  });
+  
